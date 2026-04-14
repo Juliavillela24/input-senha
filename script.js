aa04559a95e0e59452dcee6e5b5b1e0beaaf32bf
Juliavillela24 <128742756+Juliavillela24@users.noreply.github.com>
2026-04-14 15:57:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function mostrarSenha(){
         btnShowPass.classList.replace('bi-eye-slash-fill','bi-eye-fill')
     }
 }
+
+var input = document.getElementById('senha')
+var erro = document.getElementById('erro-senha')
+
+input.addEventListener('input', function () {
+    if (/[^0-9]/.test(this.value)) {
+        erro.textContent = 'A senha deve conter apenas números'
+        this.value = this.value.replace(/[^0-9]/g, '')
+    } else {
+        erro.textContent = ''
+    }
+})
